8f1983e31cdc0464025bf7f584bfb6f4cb422d52
Miu Alin-Ionut <miualinionut@yahoo.com>
2017-09-24 22:56:36 +0300

    parial 1

--- ajax/bursa/script.js
+++ ajax/bursa/script.js
@@ -1,20 +1,57 @@
-var yahoo_stocks = "https://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=&q=select * from yahoo.finance.quotes where symbol in (\"GOOGL\",\"AAPL\")";
+var stock_signs = "\"GOOGL\",\"AAPL\",\"FB\",\"MSFT\"";
+var yahoo_stocks = "https://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=&q=select * from yahoo.finance.quotes where symbol in ("+stock_signs+")";
+var curs_bnr = "http://www.bnr.ro/nbrfxrates.xml";
 
-function getStockPrice() {
+
+function getStockPrices() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var res = this.responseText;
             var json = JSON.parse(res);
-            displayStock(json);
+            displayStocks(json);
         }
     };
     xhttp.open("GET", yahoo_stocks, true);
     xhttp.send();
 }
 
-function displayStock(json) {
-    console.log(json);
+function getGetCursBnr() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var xmlDoc = xhttp.responseXML;
+            console.log(xmlDoc);
+        }
+    };
+    xhttp.open("GET", curs_bnr, true);
+    xhttp.send();
 }
 
-getStockPrice();
+function displayStocks(json) {
+    var htmlStocks = '';
+    var symbols = json.query.results.quote;
+    var size = symbols.length;
+    for(var i=0;i<size;i++) {
+        htmlStocks += getHtmlStock(symbols[i]);
+    }    
+    document.getElementById("stocks_wrapper").innerHTML = htmlStocks;
+}
+
+function getHtmlStock(stock) {
+    console.log(stock);
+    var htmlStock = `
+        <div class="stock-box">
+            <p class="stock-symbol">Symbol: ${stock.symbol}</p>
+            <hr/>
+            <p>Bid: ${stock.Bid}</p>
+            <p>Year High: ${stock.YearHigh}</p>
+            <p>Year Low: ${stock.YearLow}</p>
+            <p>Volume: ${stock.Volume}</p>
+        </div>
+    `;
+    return htmlStock;
+}
+
+getStockPrices();
+getGetCursBnr();
